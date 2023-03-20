7da17746d741a8e5e76d9568c2a843a517947b69
chenchongqiang <chongqiangchen@gmail.com>
2023-03-20 12:39:22 +0800

    fix: fix modified grammar

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,37 @@
+const webpack = require('webpack');
+
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
+  // webpack5
+  // yarn install -D @storybook/builder-webpack5 @storybook/manager-webpack5
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...(config.resolve || {}).fallback,
+        "https": require.resolve("https-browserify"),
+        "http": require.resolve("stream-http"),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer/"),
+        fs: false,
+      },
+    };
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: "process/browser",
+      })
+    )
+    // Return the altered config
+    return config;
+  },
 };
